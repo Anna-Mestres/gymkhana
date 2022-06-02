159e6f00d9253fc104a1a0c9a46998ed628b6791
Anna Mestres Casadesus <anna.mestres.casadesus@students.iaac.net>
2022-06-02 14:25:03 +0200

    add files

---
+++ data.js
@@ -0,0 +1,307 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-portada",
+      "name": "portada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -3.1351969514885987,
+        "pitch": -0.13107125770508077,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0647912912706836,
+          "pitch": 0.34829533376519173,
+          "rotation": 0,
+          "target": "1-anillas"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.1149421407239046,
+          "pitch": -0.40503370748515444,
+          "title": "Welcome to the Gymkhana",
+          "text": "ENG: This is our playing spaces. A representation of the projects we have been working along the MDEF. We hope you enjoy it!!<div><br></div><div>ESP: Estos son nuestros espacios de juego. Una representación digital de los proyectos que hemos hecho a lo largo de MDEF. ¡¡Esperamos que disfrutes!!<br><div><br></div></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-anillas",
+      "name": "anillas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -3.0017776453118508,
+        "pitch": 0.45023791029449356,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9061710398759466,
+          "pitch": 0.5541400733779369,
+          "rotation": 2.356194490192345,
+          "target": "2-pintat"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.85354763584154,
+          "pitch": 0.381305987410844,
+          "title": "Anilla(s)",
+          "text": "<div>ENG: Try to score at least one ring by throwing them on the toy stick.</div><div><br></div><div>ESP: Trata de anotar al menos una anilla tirándola dentro de los varas.&nbsp;</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-pintat",
+      "name": "pintat",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -2.799207071845583,
+        "pitch": 0.6075677372916068,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.24603667444661,
+          "pitch": 0.6179704260235894,
+          "rotation": 3.9269908169872414,
+          "target": "3-modular_furniture"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4765744820902196,
+          "pitch": 0.5254390671983238,
+          "title": "Pintat",
+          "text": "ENG: Paint without looking, something that reminds you to your childhood.&nbsp;<div><br></div><div>ESP: Pinta con los ojos cerrados algo que te recuerde a tu infancia.&nbsp;</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-modular_furniture",
+      "name": "modular_furniture",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 3.0162867377767952,
+        "pitch": 0.11823090094308597,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2592786778093057,
+          "pitch": 0.3907372170694714,
+          "rotation": 2.356194490192345,
+          "target": "4-crosshabit"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8765366134616848,
+          "pitch": -0.2143105198828028,
+          "title": "Modular Furniture",
+          "text": "ENG: Build something with all the pieces and stand still on it 5 seconds.<div><br></div><div>ESP: Construye algo con todas las piezas y luego trata de subirte encima durante 5 segundos.</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-crosshabit",
+      "name": "crosshabit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -0.13713301265669386,
+        "pitch": -0.02225258401781005,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1044243366355655,
+          "pitch": 0.31486728820048526,
+          "rotation": 3.9269908169872414,
+          "target": "5-la_diana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.18110021223598238,
+          "pitch": 0.18937732795474815,
+          "title": "Crosshabits",
+          "text": "ENG:&nbsp;Imagine how the city would be the best for all of us and fabricate it together with the participatory building parts.<div><br></div><div>ESP: Imagina como sería la mejor ciudad para todxs y fabrica con las piezas participativas de construcción.</div>"
+        }
+      ]
+    },
+    {
+      "id": "5-la_diana",
+      "name": "la_diana",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 3.0570219591182246,
+        "pitch": 0.48852519998783706,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0575230714573856,
+          "pitch": 0.5705213132398939,
+          "rotation": 8.63937979737193,
+          "target": "6-forest_game"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.062747263488948,
+          "pitch": 0.27599694912959905,
+          "title": "La Diana",
+          "text": "ENG: Throw the stones trying to get them in the center of the target.&nbsp;<div><br></div><div>ESP: Tira las piedras tratando de que queden en el centro de la diana.&nbsp;</div>"
+        }
+      ]
+    },
+    {
+      "id": "6-forest_game",
+      "name": "forest_game",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -2.9237420025692824,
+        "pitch": 0.2522634370783958,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2209987491190404,
+          "pitch": 0.4623927450918561,
+          "rotation": 3.9269908169872414,
+          "target": "7-test_portada"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.807891809865417,
+          "pitch": 0.07809297431426643,
+          "title": "Forest Game",
+          "text": "ENG: Build a sculpture that can stand for more than 10 seconds.&nbsp;<div><br></div><div>ESP: Construye una escultura que se mantenga de pie por más de 10 segundos.</div>"
+        }
+      ]
+    },
+    {
+      "id": "7-test_portada",
+      "name": "test_portada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": -0.07479982508546001,
+        "pitch": -0.534062016427324,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09973310943266256,
+          "pitch": -0.02607222546416743,
+          "rotation": 3.141592653589793,
+          "target": "0-portada"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.09973301781610289,
+          "pitch": -0.5723393758372275,
+          "title": "Thanks for playing",
+          "text": "Anna, Borka &amp; Kai"
+        }
+      ]
+    }
+  ],
+  "name": "Gymkhana MDEF",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
